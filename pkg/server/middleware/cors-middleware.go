d09317d8268f5bdff79634a49226e1f84fdf1548
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2022-06-15 20:49:18 -0300

    Handling preflight requests

--- pkg/server/middleware/cors-middleware.go
+++ pkg/server/middleware/cors-middleware.go
@@ -5,7 +5,14 @@ import "net/http"
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
